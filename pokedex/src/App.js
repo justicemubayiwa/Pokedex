d962da65af26c39e302dc1c2168644f89d3b6ea6
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-20 17:59:41 -0600

    add a constructer to handle state change

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -2,6 +2,10 @@ import React, {Component} from  'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {}
+  }
   render(){
     return(
       <div className="App">
