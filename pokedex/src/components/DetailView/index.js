107dd387937be78d0dcaca8f37c0ee49dfcc47df
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-20 19:17:36 -0600

    create detail view component. Destructure properties to be used in detail view

---
+++ pokedex/src/components/DetailView/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './DetailView.css'
+
+const DetailView = ({ pokemon }) => {
+    const { id, name, sprite, type, height, weight } = pokemon
+    return (
+        <section className="detail-view">
+        </section>
+    )
+}
+
+export default DetailView
