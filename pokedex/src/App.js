f43a966c6d2771c42941b0ce10595894e002693e
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-20 20:01:30 -0600

    fetch pokemon from api and set pokemon state = to its return

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -17,6 +17,17 @@ class App extends Component {
     }
     this.handleOnClick = this.handleOnClick.bind(this)
   }
+
+  handleOnClick(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+      .then(res => res.json())
+      .then(data => {
+        const pokemon = new Pokemon(data);
+        this.setState({ pokemon });
+      })
+      .catch(err => console.log(err));
+  }
+  
   render(){
     return(
       <div className="App">
