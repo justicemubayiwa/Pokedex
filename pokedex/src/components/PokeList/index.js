0afa024624696b43c5eb1d44d072f0006accf639
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-20 18:54:30 -0600

    import flexwrapped poke cells into Pokelist

--- pokedex/src/components/PokeList/index.js
+++ pokedex/src/components/PokeList/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import PokeCell from "../../PokeCell";
 import './PokeList.css'
 
 const PokeList = () => {
   return (
     <section className="poke-list">
+      <PokeCell />
+      <PokeCell />
+      <PokeCell />
+      <PokeCell />
 
     </section>
   )
