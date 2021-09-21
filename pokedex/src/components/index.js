f455ab481de44c6e68b73542f67abc57024c3ccf
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-20 18:27:22 -0600

    export PokeList

--- pokedex/src/components/index.js
+++ pokedex/src/components/index.js
@@ -1,12 +1,1 @@
-import React from "react";
-import './PokeList.css'
-
-const PokeList = () => {
-  return (
-    <section className="poke-list">
-
-    </section>
-  )
-}
-
-export default PokeList
+export {default as PokeList} from './PokeList'
